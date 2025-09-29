ea46dfecc22350c8895546bec1da806481fd3f7b
minkyu-types <kyuTypes96@gmail.com>
2025-09-29 18:20:35 +0900

    feat | AlarmRepository 인터페이스/구현체 임시 작성

---
+++ composeApp/alarm-data/src/commonMain/kotlin/dev/loki/alarm_data/repository/AlarmRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package dev.loki.alarm_data.repository
+
+import dev.loki.alarm.model.Alarm
+import dev.loki.alarm.repository.AlarmRepository
+
+class AlarmRepositoryImpl: AlarmRepository {
+    override suspend fun createAlarm(alarm: Alarm) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateAlarm(alarm: Alarm) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAlarm(alarm: Alarm) {
+        TODO("Not yet implemented")
+    }
+}
