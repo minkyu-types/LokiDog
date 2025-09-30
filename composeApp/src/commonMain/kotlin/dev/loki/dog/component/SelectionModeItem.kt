19a2da7d5db7f5acd41282c90e267feb7a32be1e
minkyu-types <kyuTypes96@gmail.com>
2025-09-30 17:13:32 +0900

    feat | 선택 모드 아이템 추가, 글래스판넬 아이템 추가

---
+++ composeApp/src/commonMain/kotlin/dev/loki/dog/component/SelectionModeItem.kt
@@ -0,0 +1,52 @@
+package dev.loki.dog.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
+@Composable
+fun <T> SelectionModeItem(
+    item: T,
+    isChecked: Boolean,
+    onCheckedChange: (T, Boolean) -> Unit,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = { isChecked ->
+                onCheckedChange(item, isChecked)
+            }
+        )
+        content()
+    }
+}
