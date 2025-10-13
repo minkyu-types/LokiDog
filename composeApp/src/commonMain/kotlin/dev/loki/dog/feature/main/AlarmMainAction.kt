d81c14691a9585a5915127e6035b8769591bd43f
minkyu-types <kyuTypes96@gmail.com>
2025-10-13 16:32:52 +0900

    feat | 알람 그룹 목록 화면 UI 수정 및 필터 적용, 필터 바텀 시트가 조건에 따라 보여지도록 구현

--- composeApp/src/commonMain/kotlin/dev/loki/dog/feature/main/AlarmMainAction.kt
+++ composeApp/src/commonMain/kotlin/dev/loki/dog/feature/main/AlarmMainAction.kt
@@ -1,9 +1,11 @@
 package dev.loki.dog.feature.main
 
+import dev.loki.alarmgroup.model.AlarmMainSort
 import dev.loki.dog.model.AlarmGroupModel
 import dev.loki.dog.feature.base.BaseAction
 
 sealed class AlarmMainAction: BaseAction {
+    data class SortChange(val sort: AlarmMainSort): AlarmMainAction()
     data class Add(val alarmGroup: AlarmGroupModel): AlarmMainAction()
     data class Update(val alarmGroup: AlarmGroupModel): AlarmMainAction()
     data class Delete(val alarmGroup: AlarmGroupModel): AlarmMainAction()
