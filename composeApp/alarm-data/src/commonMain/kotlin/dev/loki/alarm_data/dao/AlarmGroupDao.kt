850245c874da0c2cf2d82aed1e5d0cf41d94428e
minkyu-types <kyuTypes96@gmail.com>
2025-10-13 16:29:45 +0900

    refactor | AlarmGroup 조회 시 임시 저장된 데이터 제외하고 조회하도록 수정

--- composeApp/alarm-data/src/commonMain/kotlin/dev/loki/alarm_data/dao/AlarmGroupDao.kt
+++ composeApp/alarm-data/src/commonMain/kotlin/dev/loki/alarm_data/dao/AlarmGroupDao.kt
@@ -7,29 +7,31 @@ import androidx.room.Query
 import androidx.room.Update
 import dev.loki.alarm_data.model.AlarmGroupEntity
 import dev.loki.alarm_data.model.AlarmGroupWithAlarms
-import dev.loki.alarmgroup.model.AlarmMainSort
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AlarmGroupDao {
 
-    @Query("SELECT * FROM alarm_group ORDER BY createdAt DESC")
+    @Query("SELECT * FROM alarm_group WHERE isTemp = 0 ORDER BY createdAt DESC")
     fun getAlarmGroupsByCreated(): Flow<List<AlarmGroupEntity>>
 
-    @Query("SELECT * FROM alarm_group ORDER BY updatedAt DESC")
+    @Query("SELECT * FROM alarm_group WHERE isTemp = 0 ORDER BY updatedAt DESC")
     fun getAlarmGroupsByUpdated(): Flow<List<AlarmGroupEntity>>
 
-    @Query("SELECT * FROM alarm_group ORDER BY isActivated DESC, createdAt DESC")
+    @Query("SELECT * FROM alarm_group WHERE isTemp = 0 ORDER BY isActivated DESC, createdAt DESC")
     fun getAlarmGroupsByActivated(): Flow<List<AlarmGroupEntity>>
+
+    @Query("SELECT * FROM alarm_group WHERE isTemp = 0 ORDER  BY title DESC")
+    fun getAlarmGroupsByAlphabet(): Flow<List<AlarmGroupEntity>>
 
     @Query("SELECT * FROM alarm_group WHERE id = :id")
     fun getAlarmGroupWithAlarms(id: Long): Flow<AlarmGroupWithAlarms>
 
-    @Query("SELECT * FROM alarm_group WHERE isTemp = :isTemp ORDER BY updatedAt DESC")
-    fun getTempAlarmGroups(isTemp: Boolean = true): Flow<List<AlarmGroupEntity>>
+    @Query("SELECT * FROM alarm_group WHERE isTemp = 1 ORDER BY updatedAt DESC")
+    fun getTempAlarmGroups(): Flow<List<AlarmGroupEntity>>
 
     @Insert
-    suspend fun insert(item: AlarmGroupEntity)
+    suspend fun insert(item: AlarmGroupEntity): Long
 
     @Update
     suspend fun update(item: AlarmGroupEntity)
