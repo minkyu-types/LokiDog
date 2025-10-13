a82d8a7fb030710fd12a1a8c6ed15a27f9158cff
minkyu-types <kyuTypes96@gmail.com>
2025-10-13 16:32:52 +0900

    feat | 알람 그룹 목록 화면 UI 수정 및 필터 적용, 필터 바텀 시트가 조건에 따라 보여지도록 구현

--- composeApp/alarm-domain/src/commonMain/kotlin/dev/loki/alarmgroup/model/AlarmMainSort.kt
+++ composeApp/alarm-domain/src/commonMain/kotlin/dev/loki/alarmgroup/model/AlarmMainSort.kt
@@ -1,7 +1,8 @@
 package dev.loki.alarmgroup.model
 
-enum class AlarmMainSort {
-    MOST_RECENT_CREATED,
-    MOST_RECENT_UPDATED,
-    ACTIVATED_FIRST,
+enum class AlarmMainSort(val label: String) {
+    MOST_RECENT_CREATED("최신 등록순"),
+    MOST_RECENT_UPDATED("최신 업데이트순"),
+    ACTIVATED_FIRST("활성화순"),
+    ALPHABETICAL("알파벳순")
 }
