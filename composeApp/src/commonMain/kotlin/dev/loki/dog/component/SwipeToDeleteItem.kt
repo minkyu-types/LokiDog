e76a30e39cc9df99797aeac1d9b89a34bdb0bd33
minkyu-types <kyuTypes96@gmail.com>
2025-10-13 17:04:33 +0900

    feat | 스와이프 아이템 컴포저블 쉐입 변경

--- composeApp/src/commonMain/kotlin/dev/loki/dog/component/SwipeToDeleteItem.kt
+++ composeApp/src/commonMain/kotlin/dev/loki/dog/component/SwipeToDeleteItem.kt
@@ -3,7 +3,6 @@ package dev.loki.dog.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -11,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
@@ -64,7 +64,8 @@ fun <T> SwipeToDeleteItem(
                         .clickable {
                             onDelete(item)
                         }
-                        .background(Color.Red),
+                        .padding(horizontal = 8.dp, vertical = 20.dp)
+                        .background(Color.Red, RoundedCornerShape(20.dp)),
                     contentAlignment = Alignment.Center
                 ) {
                     Icon(
