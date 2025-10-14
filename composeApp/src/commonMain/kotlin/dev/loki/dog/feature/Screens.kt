f6a3a25bf0f0af4a4aa9e55b3b1c10d77ecfae2c
minkyu-types <kyuTypes96@gmail.com>
2025-10-14 10:13:04 +0900

    design | 메인 화면 텍스트 다국어 적용

--- composeApp/src/commonMain/kotlin/dev/loki/dog/feature/Screens.kt
+++ composeApp/src/commonMain/kotlin/dev/loki/dog/feature/Screens.kt
@@ -1,16 +1,14 @@
 package dev.loki.dog.feature
 
-sealed interface Screens {
-    val title: String
+sealed interface Screens
+
+enum class MainScreen: Screens {
+    ALARM,
+    TIMER
 }
 
-sealed class MainScreen(override val title: String): Screens {
-    data object AlarmMain: MainScreen("알람/그룹 목록")
-    data object TimerMain: MainScreen("타이머")
-}
-
-sealed class SubScreen(override val title: String): Screens {
-    data object AlarmGroupAdd: SubScreen("알람 그룹 추가")
-    data object AlarmGroupDetail: SubScreen("알람 그룹 상세")
-    data object TempAlarmGroupList: SubScreen("임시 저장 알람 그룹 목록")
+enum class SubScreen: Screens {
+    ALARM_GROUP_ADD,
+    ALARM_GROUP_DETAIL,
+    ALARM_GROUP_TEMP_LIST,
 }
