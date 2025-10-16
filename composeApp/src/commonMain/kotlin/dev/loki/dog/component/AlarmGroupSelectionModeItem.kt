ba25fb4a37e9d786a68972cf22c7eb103fe33991
minkyu-types <kyuTypes96@gmail.com>
2025-10-16 16:05:05 +0900

    feat | 공용 컴포넌트 분리

---
+++ composeApp/src/commonMain/kotlin/dev/loki/dog/component/AlarmGroupSelectionModeItem.kt
@@ -0,0 +1,83 @@
+package dev.loki.dog.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
+import dev.loki.dog.handler.DraggableItem
+import dev.loki.dog.model.AlarmGroupModel
+import dev.loki.dog.theme.OnTertiaryLight
+
+@Composable
+fun AlarmGroupSelectionModeItem(
+    index: Int,
+    isChecked: Boolean,
+    alarmGroup: AlarmGroupModel,
+    onCheckedChange: (AlarmGroupModel, Boolean) -> Unit,
+    dragController: DraggableItem? = null,
+    modifier: Modifier = Modifier
+) {
+    var itemHeightPx by remember { mutableStateOf(0f) }
+
+    SelectionModeItem(
+        item = alarmGroup,
+        isChecked = isChecked,
+        onCheckedChange = { group, checked ->
+            onCheckedChange(group, checked)
+        },
+        content = {
+            Row {
+                AlarmGroupItem(
+                    alarmGroup = alarmGroup,
+                )
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = null,
+                    tint = OnTertiaryLight
+                )
+            }
+        },
+        modifier = modifier
+            .padding(end = 16.dp)
+            .onGloballyPositioned { layoutCoordinates ->
+                itemHeightPx = layoutCoordinates.size.height.toFloat()
+            }
+            .pointerInput(itemHeightPx) {
+                detectDragGesturesAfterLongPress(
+                    onDragStart = {
+                        dragController?.onDragStart(index)
+                    },
+                    onDrag = { change, dragAmount ->
+                        change.consume()
+                        dragController?.onDrag(dragAmount.y, itemHeightPx)
+                    },
+                    onDragEnd = {
+                        dragController?.onDragEnd()
+                    },
+                    onDragCancel = {
+                        dragController?.onDragCancel()
+                    }
+                )
+            }
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                onCheckedChange(alarmGroup, !isChecked)
+            }
+            .padding(horizontal = 16.dp)
+    )
+}
---
+++ composeApp/src/commonMain/kotlin/dev/loki/dog/component/AlarmGroupSwipeToDeleteItem.kt
@@ -0,0 +1,63 @@
+package dev.loki.dog.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.sharp.ArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.loki.dog.model.AlarmGroupModel
+import dev.loki.dog.theme.OnTertiaryLight
+
+@Composable
+fun AlarmGroupSwipeToDeleteItem(
+    alarmGroup: AlarmGroupModel,
+    onAlarmGroupClick: (AlarmGroupModel) -> Unit,
+    onActivationChange: ((AlarmGroupModel) -> Unit)? = null,
+    onDelete: (AlarmGroupModel) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    SwipeToDeleteItem(
+        item = alarmGroup,
+        onDelete = onDelete,
+        content = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier
+                    .fillMaxWidth()
+            ) {
+                AlarmGroupItem(
+                    alarmGroup = alarmGroup,
+                    onActivationChange = {
+                        onActivationChange?.invoke(it)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Icon(
+                    imageVector = Icons.AutoMirrored.Sharp.ArrowRight,
+                    contentDescription = null,
+                    tint = OnTertiaryLight
+                )
+            }
+        },
+        modifier = modifier
+            .padding(end = 8.dp)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                onAlarmGroupClick(alarmGroup)
+            }
+            .padding(start = 24.dp, end = 8.dp)
+    )
+}
