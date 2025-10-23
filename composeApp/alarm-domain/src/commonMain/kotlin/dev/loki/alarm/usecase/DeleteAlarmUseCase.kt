ba109590818c0f90704f03f023dfd058824bc63d
minkyu-types <kyuTypes96@gmail.com>
2025-10-23 17:23:21 +0900

    feat | 알람 삭제 시 스케줄링 취소하도록 변경

--- composeApp/alarm-domain/src/commonMain/kotlin/dev/loki/alarm/usecase/DeleteAlarmUseCase.kt
+++ composeApp/alarm-domain/src/commonMain/kotlin/dev/loki/alarm/usecase/DeleteAlarmUseCase.kt
@@ -1,13 +1,16 @@
 package dev.loki.alarm.usecase
 
+import dev.loki.AlarmScheduler
 import dev.loki.alarm.model.Alarm
 import dev.loki.alarm.repository.AlarmRepository
 
 class DeleteAlarmUseCase(
-    private val repository: AlarmRepository
+    private val repository: AlarmRepository,
+    private val alarmScheduler: AlarmScheduler
 ) {
 
     suspend operator fun invoke(alarm: Alarm) {
         repository.deleteAlarm(alarm)
+        alarmScheduler.cancel(emptySet(),  alarm)
     }
 }
