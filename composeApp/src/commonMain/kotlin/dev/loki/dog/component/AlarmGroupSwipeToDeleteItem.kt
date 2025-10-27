2b8650a3b1d6563ffd5e3b7d210ef568b36a7a9f
minkyu-types <kyuTypes96@gmail.com>
2025-10-27 15:17:33 +0900

    refactor | 선택 모드 아이템 상태에서, 활성화 상태 스위치 색상이 유지되도록 수정

--- composeApp/src/commonMain/kotlin/dev/loki/dog/component/AlarmGroupSwipeToDeleteItem.kt
+++ composeApp/src/commonMain/kotlin/dev/loki/dog/component/AlarmGroupSwipeToDeleteItem.kt
@@ -3,10 +3,8 @@ package dev.loki.dog.component
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.sharp.ArrowRight
 import androidx.compose.material3.Icon
