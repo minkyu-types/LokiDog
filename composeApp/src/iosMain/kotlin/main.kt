cd11440900bad23a94f52097197471895070c838
minkyu-types <kyuTypes96@gmail.com>
2025-10-30 13:53:50 +0900

    design | 리소스 데이터 추가

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,7 +1,11 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.loki.dog.App
+import dev.loki.dog.expect.PlatformAlarmScheduler
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController {
+    // iOS 알람 카테고리 초기화
+    PlatformAlarmScheduler.setupNotificationCategories()
+
     return ComposeUIViewController { App() }
 }
