e839279a974f93bed5d130688393cc0de4e1682f
minkyu-types <kyuTypes96@gmail.com>
2025-11-03 15:42:52 +0900

    test | 파라미터 제거

--- composeApp/alarm-domain/src/commonTest/kotlin/dev/loki/alarm/usecase/alarm/UpsertAlarmUseCaseTest.kt
+++ composeApp/alarm-domain/src/commonTest/kotlin/dev/loki/alarm/usecase/alarm/UpsertAlarmUseCaseTest.kt
@@ -1,7 +1,6 @@
 package dev.loki.alarm.usecase.alarm
 
 import dev.loki.alarm.FakeAlarmRepository
-import dev.loki.alarm.FakeAlarmScheduler
 import dev.loki.alarm.model.Alarm
 import dev.loki.alarm.usecase.UpsertAlarmUseCase
 import kotlinx.coroutines.test.runTest
@@ -16,8 +15,7 @@ class UpsertAlarmUseCaseTest {
     fun `제공된 알람으로 repository의 upsertAlarm을 호출해야 한다`() = runTest {
         // Given
         val mockRepository = FakeAlarmRepository()
-        val mockScheduler = FakeAlarmScheduler()
-        val useCase = UpsertAlarmUseCase(mockRepository, mockScheduler)
+        val useCase = UpsertAlarmUseCase(mockRepository)
 
         val testAlarm = Alarm(
             id = 1L,
@@ -41,8 +39,7 @@ class UpsertAlarmUseCaseTest {
     fun `반복 요일이 없는 알람을 처리할 수 있어야 한다`() = runTest {
         // Given
         val mockRepository = FakeAlarmRepository()
-        val mockScheduler = FakeAlarmScheduler()
-        val useCase = UpsertAlarmUseCase(mockRepository, mockScheduler)
+        val useCase = UpsertAlarmUseCase(mockRepository)
 
         val testAlarm = Alarm(
             id = 2L,
@@ -65,8 +62,7 @@ class UpsertAlarmUseCaseTest {
     fun `임시 알람을 처리할 수 있어야 한다`() = runTest {
         // Given
         val mockRepository = FakeAlarmRepository()
-        val mockScheduler = FakeAlarmScheduler()
-        val useCase = UpsertAlarmUseCase(mockRepository, mockScheduler)
+        val useCase = UpsertAlarmUseCase(mockRepository)
 
         val tempAlarm = Alarm(
             id = 0L,
@@ -90,8 +86,7 @@ class UpsertAlarmUseCaseTest {
     fun `모든 요일이 설정된 알람을 처리할 수 있어야 한다`() = runTest {
         // Given
         val mockRepository = FakeAlarmRepository()
-        val mockScheduler = FakeAlarmScheduler()
-        val useCase = UpsertAlarmUseCase(mockRepository, mockScheduler)
+        val useCase = UpsertAlarmUseCase(mockRepository)
 
         val dailyAlarm = Alarm(
             id = 3L,
@@ -114,8 +109,7 @@ class UpsertAlarmUseCaseTest {
     fun `여러 알람을 순차적으로 처리할 수 있어야 한다`() = runTest {
         // Given
         val mockRepository = FakeAlarmRepository()
-        val mockScheduler = FakeAlarmScheduler()
-        val useCase = UpsertAlarmUseCase(mockRepository, mockScheduler)
+        val useCase = UpsertAlarmUseCase(mockRepository)
 
         val alarm1 = Alarm(1L, 100L, "08:00", "First", true, false)
         val alarm2 = Alarm(2L, 100L, "09:00", "Second", true, false)
@@ -137,8 +131,7 @@ class UpsertAlarmUseCaseTest {
     fun `기존 알람을 업데이트할 수 있어야 한다`() = runTest {
         // Given
         val mockRepository = FakeAlarmRepository()
-        val mockScheduler = FakeAlarmScheduler()
-        val useCase = UpsertAlarmUseCase(mockRepository, mockScheduler)
+        val useCase = UpsertAlarmUseCase(mockRepository)
 
         val originalAlarm = Alarm(1L, 100L, "08:00", "Original", true, false)
         val updatedAlarm = Alarm(1L, 100L, "08:30", "Updated", true, false)
